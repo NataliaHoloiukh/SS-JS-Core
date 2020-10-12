eecb2cad874fd68e8c407a0907e9a0c3bd83bf4c
unknown <revolveng91@gmail.com>
2020-10-12 22:31:14 +0300

    changed task3

--- lesson 9 jQuery/script.js
+++ lesson 9 jQuery/script.js
@@ -5,9 +5,14 @@ $('a[href^="https://"]').attr('target', '_blank');
 // **********Task 2*********
 $('h2.head').css('background', 'green').find('.inner').css('fontSize', '35px');
 
+
 // **********Task 3*********
-let x = $('h3+div');
-$('h3').prepend(x);
+let div1 = $('h3+div:first');
+let div2 = $('h3+div:last');
+
+$('h3:first').before(div1);
+$('h3:last').before(div2);
+
 
 // **********Task 4*********
 let checkbox = $('input');
